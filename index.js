eb67c781b9d369a9f59bc0a8f8ed7e0835f10c1e
Vidit <viditgoel2k@gmail.com>
2024-04-18 14:12:40 +0530

    init: server setup

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+
+app.use("/api", (req, res, next) => {
+    res.send("Hello World");
+});
+
+app.listen(5000, () => {
+    console.log("Listening on localhost port: 5000");
+});
